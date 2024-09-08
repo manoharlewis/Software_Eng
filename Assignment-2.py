5ff7a6f1fea19bb5067fb7611ef5463c288db79f
manoharlewis <dsaisandesh@gmail.com>
2024-09-08 17:41:13 -0500

    add open comments

--- Assignment-2.py
+++ Assignment-2.py
@@ -1,7 +1,18 @@
 import math
 
 
-# Fermat's Last Theorem Near Miss Finder
+#Fermat's Last Theorem Near Miss Finder
+#File Name : Assignment-2
+#Course and Section Number : FA24-CPSC-60500-2
+#COMPLETION DATE : 09/08/2024
+"""This program searches for "near misses" to Fermat's Last Theorem by checking 
+if x^n + y^n comes close to any z^n values for given integers x, y, z, and n. 
+The user provides the power n and the upper limit k for x and y. 
+The program calculates the relative miss between x^n + y^n and the closest z^n or (z+1)^n. 
+It tracks and prints the smallest miss found, along with the corresponding values of x, y, and z. 
+The goal is to identify cases where x^n + y^n almost equals z^n for powers greater than 2.
+"""
+
 
 def find_near_misses(n, k):
     """This function searches for near misses of Fermat's Last Theorem."""
